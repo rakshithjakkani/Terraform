8383d8bcb796481a25b71f294b95d52d284d5e22
Rakshith Jakkani <rakshithjakkani@fynd-external.com>
2024-12-13 11:36:32 +0530

    readycode for s3 static website hosting

--- Local-modules-AWS/modules/aws-s3-static-website-bucket/main.tf
+++ Local-modules-AWS/modules/aws-s3-static-website-bucket/main.tf
@@ -3,6 +3,14 @@ resource "aws_s3_bucket" "s3_bucket" {
 
   tags = var.tags
 }
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_website_configuration" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
@@ -16,14 +24,9 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  acl = "public-read"
-}
-
 resource "aws_s3_bucket_policy" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.example]
 
   policy = jsonencode({
     Version = "2012-10-17"
